e68a7bd7460415f01f4f4e5dd40cce9e778a1511
App Academy Student <student@appacademy.io>
2016-01-13 11:19:03 -0800

    two_sum finds pairs of positions equal to zero and tests correctly

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -4,4 +4,16 @@ class Array
     self.each { |el| result << el unless result.include?(el) }
     result
   end
+
+  def two_sum
+    results = []
+    self.each.with_index do |el, idx|
+      pair = []
+      (idx + 1).upto(self.length - 1) do |jdx|
+        pair << [idx, jdx]  if el + self[jdx] == 0
+      end
+      results += pair unless pair.empty?
+    end
+    results
+  end
 end
--- spec/spec_exercises.rb
+++ spec/spec_exercises.rb
@@ -8,5 +8,18 @@ describe "#my_uniq" do
   it 'returns an array with unique elements' do
     expect(array.my_uniq).to eq([1,2,3,4,5])
   end
-  
+
+end
+
+describe "#two_sum" do
+  let(:array) {[-1, 0, 2, -2, 1]}
+
+  it 'return pairs of positions for elements equal to zero' do
+    expect(array.two_sum).to eq([[0, 4], [2, 3]])
+  end
+
+  it 'returns results sorted' do
+    expect(array.two_sum.sort).to eq(array.two_sum)
+  end
+
 end
